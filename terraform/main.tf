7f0e4cb84f3fbf63fdcfa42e00560299b448ce70
Benjamin Schneider <bschnei@gmail.com>
2021-10-03 13:04:14 -0700

    allow minor version upgrades for gcp provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.69.0"
+      version = "~> 3.69"
     }
     namecheap = {
       source  = "robgmills/namecheap"
